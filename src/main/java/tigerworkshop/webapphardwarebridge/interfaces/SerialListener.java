ea107d7be98c50fbf6c711fca1ca94dc1c19abcf
Tiger Fok <tiger.fok@indzz.cn>
2018-12-27 17:19:44 +0800

    Cleanup

--- src/main/java/tigerworkshop/webapphardwarebridge/interfaces/SerialListener.java
+++
@@ -1,9 +0,0 @@
-package tigerworkshop.webapphardwarebridge.interfaces;
-
-import tigerworkshop.webapphardwarebridge.websocketservices.SerialWebSocketService;
-
-public interface SerialListener {
-    void onStart(SerialWebSocketService serialWebSocketService);
-
-    void onDataReceived(SerialWebSocketService serialWebSocketService, String receivedData);
-}
