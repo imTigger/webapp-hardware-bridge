4c2b5e7aa703fb6fbce8b105da341d24fe8ea5fd
Tiger Fok <tiger.fok@indzz.cn>
2019-03-15 16:33:21 +0800

    Fixed missing ObservableStringPair problem

---
+++ configurator/src/main/java/tigerworkshop/webapphardwarebridge/config/models/ObservableStringPair.java
@@ -0,0 +1,44 @@
+package tigerworkshop.webapphardwarebridge.config.models;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class ObservableStringPair {
+    private StringProperty left = new SimpleStringProperty();
+    private StringProperty right = new SimpleStringProperty();
+
+    public ObservableStringPair(String left, String right) {
+        setLeft(left);
+        setRight(right);
+    }
+
+    public static ObservableStringPair of(String left, String right) {
+        return new ObservableStringPair(left, right);
+    }
+
+    public String getLeft() {
+        return left.get();
+    }
+
+    public void setLeft(String left) {
+        this.left.set(left);
+    }
+
+    public StringProperty leftProperty() {
+        return left;
+    }
+
+    public String getRight() {
+        return right.get();
+    }
+
+    public void setRight(String right) {
+        this.right.set(right);
+    }
+
+    public StringProperty rightProperty() {
+        return right;
+    }
+}
+
+
