674ff3870cce901d65e84a9a24a302a5c0d406c6
Tiger Fok <tiger@tiger-workshop.com>
2024-07-25 23:59:42 +0100

    Updated version number

--- src/main/java/tigerworkshop/webapphardwarebridge/Constants.java
+++ src/main/java/tigerworkshop/webapphardwarebridge/Constants.java
@@ -3,5 +3,5 @@ package tigerworkshop.webapphardwarebridge;
 public class Constants {
     public static final String APP_NAME = "WebApp Hardware Bridge";
     public static final String APP_ID = "tigerworkshop.webapphardwarebridge";
-    public static final String VERSION = "1.0.0-alpha3";
+    public static final String VERSION = "1.0.0";
 }
